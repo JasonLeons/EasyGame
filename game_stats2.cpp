e57365348f1f02050245992d0998451bda822b36
Leon <JasonLeons@outlook.com>
2025-01-03 22:43:42 +0700

    change

--- game_stats2.cpp
+++ game_stats2.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    /*
     const int ALIEN_POINTS = 100;
     int aliensKills = 10;
     int score = aliensKills * ALIEN_POINTS;
@@ -20,5 +21,46 @@ int main(int argc, char const *argv[])
 
     cout << "To upgrade my ship to a Cruiser will cost " << 
     (CRUISER_COST - myShipCost) << " Resources Points." << endl;
+    
+
+    const int GOLD_PIECES = 900;
+    int adventurers, killed, survivors;
+    string leader;
+
+    // get the information
+
+    cout << "Welcome to Lost Fortune!" << endl;
+    cout << "Please enter the following for your personalized adverture\n";
+
+    cout << "Enter the number: ";
+    cin >> adventurers;
+
+    cout << "Enter the number, smaller than the first: ";
+    cin >> killed;
+
+    cout << "Enter the last name: ";
+    cin >> leader;
+
+    //tell the story
+    cout << "\nA brave group of " << adventurers << " set out on a quest ";
+    cout << " -- in search of the lost treasure of the Ancient Dwarves. ";
+    cout << "The group was led by that legendary rogue, " << leader << ".\n";
+    cout << "\nAlong the way, a band of marauding ogres ambushed the party. ";
+    cout << "All fought bravely under the command of " << leader;
+    cout << ", and the ogres were defeated, but at a cost. ";
+    cout << "Of the adventurers, " << killed << " were vanquished, ";
+    cout << "leaving just " << survivors << " in the group.\n";
+
+    cout << "\nThe party was about to give up all hope. ";
+    cout << "But while laying the deceased to rest, ";
+    cout << "they stumbled upon the buried fortune. ";
+    cout << "So the adventurers split " << GOLD_PIECES << " gold pieces.";
+    cout << leader << " held on to the extra " << (GOLD_PIECES % survivors) ;
+    cout << " pieces to keep things fair of course.\n";
+    */
+
+    cout << "Seven divided by three is " << 7 / 3 << endl;
+    cout << "Seven divided by three is " << 7.0 / 3 << endl;
+    cout << "Seven divided by three is " << 7.0 / 3.0 << endl;
     return 0;
 }
